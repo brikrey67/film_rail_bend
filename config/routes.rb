05208d76fdbca5c7372deaf186fd326040e537c5
Brian Reynolds <brikrey@gmail.com>
2018-02-22 15:41:46 -0500

    minor route refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do 
   namespace :api do 
     namespace :v1 do 
-      resources :items, only: [:index, :show, :create, :destroy, :update] 
+      resources :films, only: [:index, :show, :create, :destroy, :update] 
     end 
   end 
 end
 
-Read more at https://www.pluralsight.com/guides/ruby-ruby-on-rails/building-a-crud-interface-with-react-and-ruby-on-rails#TVkVsTKbla0ZQCM4.99
+# Read more at https://www.pluralsight.com/guides/ruby-ruby-on-rails/building-a-crud-interface-with-react-and-ruby-on-rails#TVkVsTKbla0ZQCM4.99
